b65c7698f19489f3a376941eda5b543784df94b9
BudacaB <budaca.b@gmail.com>
2020-01-26 21:28:02 +0200

    string comparison

---
+++ String/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    char letter1 = 'A';
+//	    char letter2 = 'A';
+//        System.out.println(letter1 == letter2);
+
+        // 'new' makes separate memory space / without new they would both point to the same location - java optimization
+        String letter1 = new String("A");
+        String letter2 = new String("A");
+
+        // compares pointers / memory address
+        System.out.println(letter1 == letter2);
+
+        // compares internals
+        System.out.println(letter1.equals(letter2));
+    }
+}
