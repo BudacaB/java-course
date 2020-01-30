68b92fda89f00e82abea9229d157446a0a892621
BudacaB <budaca.b@gmail.com>
2020-01-31 00:43:53 +0200

    string literals

--- String/src/com/company/Main.java
+++ String/src/com/company/Main.java
@@ -6,21 +6,30 @@ public class Main {
 //	    char letter1 = 'A';
 //	    char letter2 = 'A';
 //        System.out.println(letter1 == letter2);
+//
+//        // 'new' makes separate memory space / without new they would both point to the same location - java optimization
+//        String letter3 = new String("A");
+//        String letter4 = new String("A");
+//
+//        // compares pointers / memory address
+//        System.out.println(letter3 == letter4);
+//
+//        // compares internals
+//        System.out.println(letter3.equals(letter4));
+//
+//        String name1 = "Jean";
+//        String name2 = "JEAN";
+//
+//        System.out.println(name1.equalsIgnoreCase(name2));
+//        System.out.println(name1.length());
 
-        // 'new' makes separate memory space / without new they would both point to the same location - java optimization
-        String letter1 = new String("A");
-        String letter2 = new String("A");
+        String orig = "xyz";
+        String dup = "xyz";
+        String other = new String("xyz");
 
-        // compares pointers / memory address
-        System.out.println(letter1 == letter2);
-
-        // compares internals
-        System.out.println(letter1.equals(letter2));
-
-        String name1 = "Jean";
-        String name2 = "JEAN";
-
-        System.out.println(name1.equalsIgnoreCase(name2));
-        System.out.println(name1.length());
+        System.out.println(orig == dup);
+        System.out.println(orig == other);
+        System.out.println(orig.equals(dup));
+        System.out.println(orig.equals(other));
     }
 }
