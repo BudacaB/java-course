172e5bf39bfeb44d8af42bf39fe35aea1cec2ef1
BudacaB <budaca.b@gmail.com>
2020-02-16 20:29:21 +0200

    chapter5

---
+++ Chapter5/src/chapter5/Main.java
@@ -0,0 +1,28 @@
+package chapter5;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    short a = 1;
+//	    short b = 2;
+//	    short c = 3;
+//	    int d = a + b + c;
+//
+//        System.out.println(d);
+
+		int speed = 35;
+		int speedLimit = 30;
+
+		System.out.println("Speed: " + speed);
+		System.out.println("Speed limit: " + speedLimit);
+
+		boolean result = speed < speedLimit;
+		System.out.println("speed < speedLimit results in: ");
+		System.out.println(result);
+
+		result = speed > speedLimit;
+		System.out.println("speed > speedLimit results in: ");
+		System.out.println(result);
+
+    }
+}
--- notes.md
+++ notes.md
@@ -18,3 +18,23 @@
 The main difference between heap and stack is that stack memory is used to store local variables and function call while heap memory is used to store objects in Java.
 
 ---
+
+Expressions are a combination of variables, literals and operators
+
+5 - x
+
+5 -> literal
+
+' - ' -> operator
+
+x -> variable
+
+In Java add a ';' semicolon at the end of the line and you get a statement
+
+' = ' -> assignment operator
+
+! Left operand must be a variable in Java (x = 5)
+
+! Evaluation is done right to left
+
+! If you need a float division, make sure at least one of the operands is defined as a float
