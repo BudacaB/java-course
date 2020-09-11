115d9ec107b42a6947cc1c4cf1aeec269776d4dc
BudacaB <budaca.b@gmail.com>
2020-09-12 02:36:13 +0300

    byte array <-> binary file

---
+++ New/src/course/chapter15/Read.java
@@ -0,0 +1,58 @@
+package course.chapter15;
+
+import java.io.*;
+
+public class Read {
+
+    static byte[] bytes;
+
+    public static void main(String[] args) {
+        String filename = "/home/bogdan/Pictures/ranked.png";
+        String filename2 = "/home/bogdan/Pictures/ranked2.png";
+
+        readFileIntoArray(filename);
+        writeArrayToFile(filename2);
+
+    }
+
+    static void readFileIntoArray(String fn) {
+        FileInputStream fis = null;
+        File f = new File(fn);
+        long length = f.length();
+        System.out.println(length);
+
+        try {
+            fis = new FileInputStream(f);
+            bytes = new byte[(int) length];
+            fis.read(bytes);
+            System.out.println(bytes);
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+
+    static void writeArrayToFile(String fn) {
+        FileOutputStream fos = null;
+        File f = new File(fn);
+
+        try {
+            fos = new FileOutputStream(f);
+            fos.write(bytes);
+            System.out.println(bytes);
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+}
